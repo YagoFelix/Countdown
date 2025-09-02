f8813617d9cee1d1fe0037f8e22d4a4a181da4e0
Yago <y.felixdasilva@gmail.com>
2025-09-02 09:45:06 -0300

    feat: add Counter.css, add Title.css, finished css

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -8,8 +8,8 @@ import './App.css'
 function App() {
 
   return (
-    <div className='App'>
-      <div className="container" style={{backgroundImage: `url(${NewYear})`}}>
+    <div className='App' style={{backgroundImage: `url(${NewYear})`}}>
+      <div className="container" >
         <Title title="Contagem regressiva para 2026" />
         <div className="countdown-container">
           <Counter title='Dias' number={2} />
